8c4b205f51711990116a4be8d5dc84c4a35b32aa
Aaisha Munir <k250031@nu.edu.pk>
2025-10-18 17:56:02 +0500

    Update q6.c

--- q6.c
+++ q6.c
@@ -2,9 +2,9 @@
 int main(){
 	int vowels=0, consonants=0,i=0;
 	char str[30];
-	printf("Enter a word:"); \\assume the user is entering alpha characs only
+	printf("Enter a word:"); //assume the user is entering alpha characs only
 	scanf("%s", str);
-	while(str[i]!= '\0'){ \\counts for one word, words more than 1 would be ignored 
+	while(str[i]!= '\0'){ //counts for one word, words more than 1 would be ignored 
 		if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i'|| str[i] == 'o'|| str[i] =='u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I'|| str[i] == 'O'|| str[i] =='U'){
 			vowels = vowels +1;
 		}
@@ -17,5 +17,6 @@ int main(){
 	printf("Number of consonants your word contains are %d", consonants);
 	return 0;
 }
+
 
 
